4b7986d7493f8450edb0fae7a9282a42f0e25921
Michael DeWitt <md@hackfoolery.com>
2012-11-03 11:04:34 -0400

    remove default index

--- course_feedback/config/routes.rb
+++ course_feedback/config/routes.rb
@@ -2,6 +2,8 @@ CourseFeedback::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  resources :courses
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -48,7 +50,7 @@ CourseFeedback::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'courses#index'
 
   # See how all your routes lay out with "rake routes"
 
