e5f69c56167ab531149a48092c42e0e11469f369
Michael DeWitt <md@hackfoolery.com>
2012-11-03 10:02:26 -0400

    Blog app from first half.
    Mostly just followed the tutorial at
    guides.rubyonrails.org/getting_started.html.

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '45f8dae85d1eb987d53975aa147dc1b7e1cc845a605ad784e41cb2d7d4c027542c7a3dc708999c620de699dd65e9d190a32868511a8811fb3912c89489bbe7f4'
