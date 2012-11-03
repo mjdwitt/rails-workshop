a09f21a7af6f70ce07fffdcb2a08fdbf2c61bce1
Michael DeWitt <md@hackfoolery.com>
2012-11-03 10:03:13 -0400

    scaffolding for implementation of feedback app

---
+++ course_feedback/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseFeedback::Application.config.secret_token = 'db1d92370d1d93aff03cf47b1febc9fd51312924c245049da1603d0527324b3890a4bc7c8be0ade4a81ad1e0ab98b48f0954e6a660033932158d2de2c0799603'
